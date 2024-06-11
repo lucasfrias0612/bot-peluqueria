fedd4baa034f8ea7beb4244673266f3766122464
uciel <ucibustamante.a@gmail.com>
2024-06-11 12:45:19 -0300

    Validacion de nombre

--- flows/welcome.flows.ts
+++ flows/welcome.flows.ts
@@ -2,10 +2,15 @@ import { addKeyword, EVENTS } from "@builderbot/bot";
 import { BotContext } from "@builderbot/bot/dist/types";
 import { flowSeller } from "./seller.flow";
 
+const validateName = (name: string) => {
+    const nameRegex = /^[a-zA-Z\s]+$/;
+    return nameRegex.test(name);
+};
+
 const welcomeFlow = addKeyword(EVENTS.WELCOME)
     .addAnswer(['¡Hola! Soy *TOSiBot* 😀, el asistente virtual de *The Old School Services*.'], null, async (context: BotContext, { flowDynamic, gotoFlow, state }) => {
         if (!state.get('name')) {
-            if (context.name) {
+            if (context.name && validateName(context.name)) {
                 return await flowDynamic(`¿Tu nombre es ${context.name}?`);
             } else {
                 return await flowDynamic('¿Cuál es tu nombre?');
@@ -15,16 +20,29 @@ const welcomeFlow = addKeyword(EVENTS.WELCOME)
         }
     })
     .addAction({ capture: true }, async (context: BotContext, { flowDynamic, gotoFlow, state }) => {
-        if (context.body.trim().toLowerCase().includes('si') || context.body.trim().toLowerCase().includes('yes')) {
-            await state.update({ name: context.name });
-            // Request POST a la API para generar un contact
-            return gotoFlow(flowSeller);
+        const userResponse = context.body.trim().toLowerCase();
+        if (userResponse.includes('si') || userResponse.includes('yes')) {
+            const name = state.get('name');
+            if (validateName(name)) {
+                await state.update({ name });
+                // Request POST a la API para generar un contact
+                return gotoFlow(flowSeller);
+            } else {
+                return await flowDynamic('Por favor, ingresa un nombre válido (solo letras y espacios). ¿Cuál es tu nombre?');
+            }
         } else {
             return await flowDynamic('¿Cuál es tu nombre?');
         }
     })
     .addAction({ capture: true }, async (context: BotContext, { flowDynamic, gotoFlow, state }) => {
-        await state.update({ name: context.body });
+        let name = context.body.trim();
+
+        while (!validateName(name)) {
+            await flowDynamic('Por favor, ingresa un nombre válido (solo letras y espacios). ¿Cuál es tu nombre?');
+            name = await state.get('body')
+        }
+
+        await state.update({ name: name });
         // Request POST a la API para generar un contact
         return gotoFlow(flowSeller);
     });
